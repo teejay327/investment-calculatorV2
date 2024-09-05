852e28c9c859aa16a5614a6391a4211cdef37f9d
teejay327 <mrenergy@hotmail.com>
2024-09-05 23:42:17 +1000

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+const Header = () => {
+
+  return (
+    <header>
+      <img src="{logo}" />
+      <h1> Investment Calculator</h1>
+    </header>
+  )
+};
+
+export default Header;
