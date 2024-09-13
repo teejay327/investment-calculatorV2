50a2e05bcd2b943e0d01e540aced5efd6721f579
teejay327 <mrenergy@hotmail.com>
2024-09-14 00:18:11 +1000

    Connected results component to App.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,10 @@
+import { calculateInvestmentResults } from '../util/investment.js';
+
 const Results = ({ input }) => {
 
-  console.log (input);
+  const resultsData = calculateInvestmentResults( input );
+  console.log(resultsData);
+
   return (
     <p>Results ...</p>
   )
